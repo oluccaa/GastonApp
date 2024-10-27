1fe46c4062b721ad4fa99844e321b08dfd038d31
Gustavo M. de Farias <gumf2004@outlook.com>
2024-10-27 20:27:30 -0300

    Adicionando uma tela para as notificações.

--- app/src/main/java/com/example/gaston/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/gaston/ui/fragment/HomeFragment.kt
@@ -2,8 +2,10 @@ package com.example.gaston.ui.fragment
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.gaston.database.AppDatabase
 import com.example.gaston.databinding.FragmentHomeBinding
+import com.example.gaston.ui.activity.TelaNotificacao // Certifique-se de importar a Activity correta
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -43,21 +46,23 @@ class HomeFragment : Fragment() {
 
         sharedPreferences = requireContext().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
 
+        // Configurar o clique do ImageButton
+        binding.btnnotificacoes.setOnClickListener {
+            val intent = Intent(activity, TelaNotificacao::class.java)
+            startActivity(intent)
+        }
+
         // Obtém os dados dos argumentos e atualiza a UI
         val renda = arguments?.getDouble("ACUSA_RENDA") ?: 0.0
         val totalOrcamento = arguments?.getDouble("VALOR_ORCAMENTO") ?: 0.0
         val saldoRestante = arguments?.getDouble("SALDO_RESTANTE") ?: 0.0
 
-
         if (renda > 0.0 || totalOrcamento > 0.0 || saldoRestante > 0.0) {
-            // Se houver valores de orçamento ou renda nos argumentos, atualize a UI
             updateUI(renda, totalOrcamento, saldoRestante)
         } else {
-            // Caso contrário, apenas atualize a UI com os valores atuais do banco de dados
             updateUIFromDatabase()
         }
     }
-
 
     private fun updateUI(renda: Double, totalOrcamento: Double, saldoRestante: Double) {
         lifecycleScope.launch {
@@ -69,19 +74,14 @@ class HomeFragment : Fragment() {
                     dao.getAllReceitas().sumOf { it.valor }
                 }
 
-                // Utiliza a função calcularSaldo para obter o novo saldo restante e o saldo diário
                 val (novoSaldoRestante, saldoDiario) = calcularSaldo(totalOrcamento, totalDespesas, totalReceitas)
 
-
-                // Atualiza a UI com os valores calculados
                 binding.textViewTotalSaldoDiario.text = "R$ ${String.format("%.2f", saldoDiario)}"
                 binding.textViewTotalSaldo.text = "R$ ${String.format("%.2f", totalOrcamento)}"
                 binding.textViewTotalDespesas.text = "- R$ ${String.format("%.2f", totalDespesas)}"
                 binding.textViewTotalReceitas.text = "+ R$ ${String.format("%.2f", totalReceitas)}"
                 binding.textView10.text = "R$ ${String.format("%.2f", novoSaldoRestante)}"
 
-                // Salva o valor calculado no SharedPreferences
-                sharedPreferences.edit().putFloat("VALOR_ORCAMENTO", totalOrcamento.toFloat()).apply()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
@@ -99,39 +99,30 @@ class HomeFragment : Fragment() {
                     dao.getAllReceitas().sumOf { it.valor }
                 }
                 val totalOrcamentoCalculado = withContext(Dispatchers.IO) {
-                    daoR.getOrcamento() ?: 0.0 // Obtém o orçamento mais recente
+                    daoR.getOrcamento() ?: 0.0
                 }
 
-                // Utiliza a função calcularSaldo para obter o novo saldo restante e o saldo diário
                 val (novoSaldoRestante, saldoDiario) = calcularSaldo(totalOrcamentoCalculado, totalDespesas, totalReceitas)
 
-                // Atualiza a UI com os valores calculados
                 binding.textViewTotalSaldoDiario.text = "R$ ${String.format("%.2f", saldoDiario)}"
                 binding.textViewTotalSaldo.text = "R$ ${String.format("%.2f", totalOrcamentoCalculado)}"
                 binding.textViewTotalDespesas.text = "- R$ ${String.format("%.2f", totalDespesas)}"
                 binding.textViewTotalReceitas.text = "+ R$ ${String.format("%.2f", totalReceitas)}"
                 binding.textView10.text = "R$ ${String.format("%.2f", novoSaldoRestante)}"
 
-                // Salva o valor calculado no SharedPreferences
-                sharedPreferences.edit().putFloat("VALOR_ORCAMENTO", totalOrcamentoCalculado.toFloat()).apply()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun calcularSaldo(totalOrcamento: Double, totalDespesas: Double, totalReceitas: Double): Pair<Double, Double> {
-        // Calcula o saldo restante
+    fun calcularSaldo(totalOrcamento: Double, totalDespesas: Double, totalReceitas: Double): Pair<Double, Double> {
         val novoSaldoRestante = (totalOrcamento - totalDespesas) + totalReceitas
-
-        // Evita saldo diário negativo
         val saldoDiario = if (novoSaldoRestante > 0) {
             novoSaldoRestante / 30
         } else {
             0.0
         }
-
-        // Retorna ambos os valores como um Pair (saldo restante, saldo diário)
         return Pair(novoSaldoRestante, saldoDiario)
     }
 
